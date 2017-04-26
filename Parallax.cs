52ca2bae6f922472e85bd0123e31cd972565cd81
Steve Lux <sales@atomilux.com>
2017-04-26 17:20:13 -0500

    Background scroller that can handle X number of layers. Publicly exposed array for GameObjects and float for offset amount.

---
+++ Parallax.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Parallax : MonoBehaviour {
+
+	public GameObject[] parallaxObjs;
+
+	float parallaxMultiplier = -0.01f;
+	
+	void FixedUpdate() {
+		setParallax(gameObject.GetComponent<Rigidbody2D>().velocity.x);
+	}
+
+	//int direct is either 1,0 or -1
+	public void setParallax(float velocityX) {
+
+		//Debug.Log ("setParallax() - velocityX: " + velocityX);
+
+		if (parallaxObjs.Length == 0) {
+			return;
+		}
+
+		float parallaxMultFinal = parallaxMultiplier;
+
+		if (velocityX == 0) {
+			return;
+		}
+
+		//invert if negative
+		if (velocityX < 0) {
+			parallaxMultFinal = -parallaxMultiplier;
+			//Debug.Log ("setParallax() - INVERTED: " + parallaxMultFinal);
+		}
+
+		for (int i=0; i<parallaxObjs.Length; i++) {
+
+			float mover = (i+1)*parallaxMultFinal;
+			
+			//Debug.Log ("setParallax() - mover: " + mover);
+			
+			//get v3 of BG
+			Vector3 tmp = parallaxObjs[i].transform.position;
+			
+			//Debug.Log ("setParallax() - original parallax X: " + tmp.x);
+			
+			tmp.x += mover;
+			
+			//Debug.Log ("setParallax() - new parallax X: " + tmp.x);
+			
+			//reassign V3 and update loc
+			parallaxObjs[i].transform.position = tmp;
+
+		}
+
+	}
+
+}
